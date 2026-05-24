7c6b1e4ba38fa37b3fcba276eaabcf6533de8525
Ubuntu <n8nadmin@n8n-vm.uumawk5n22gubbgmp2dehdzldf.ax.internal.cloudapp.net>
2026-05-24 09:36:45 +0000

    Fix static-export logo image paths

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const basePath = rawBasePath && rawBasePath !== "/" ? rawBasePath.replace(/\/$/,
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   ...(basePath
     ? {
         basePath,
